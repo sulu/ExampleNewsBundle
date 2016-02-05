a0c4a3c185be2b353c403c8adf38b93b2ec2dd99
Johannes Wachter <johannes.wachter@massiveart.at>
2016-02-05 15:13:09 +0100

    added template and rendering call

--- Resources/public/js/components/news/form/main.js
+++ Resources/public/js/components/news/form/main.js
@@ -1,5 +1,16 @@
-define(function() {
+define(['text!./form.html'], function(form) {
+
     return {
+
+        defaults: {
+            templates: {
+                form: form
+            },
+            translations: {
+                title: 'public.title',
+                content: 'news.content'
+            }
+        },
 
         header: {
             title: 'news.headline',
@@ -21,7 +32,21 @@ define(function() {
         },
 
         initialize: function() {
-            this.$el.html('<h1>Hello awesome sulu world</h1>');
+            this.render();
+
+            this.bindDomEvents();
+        },
+
+        render: function() {
+            this.$el.html(this.templates.form({translations: this.translations}));
+
+            this.sandbox.form.create('#news-form');
+        },
+
+        bindDomEvents: function() {
+            this.$el.find('input, textarea').on('keypress', function() {
+                this.sandbox.emit('sulu.header.toolbar.item.enable', 'save');
+            }.bind(this));
         }
     };
 });
