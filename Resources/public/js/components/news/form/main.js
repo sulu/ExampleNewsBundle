dba31e27b9d517e5246ae9d4ffca18d08bf55408
Johannes Wachter <johannes.wachter@massiveart.at>
2016-02-12 08:07:10 +0100

    finished add form

--- Resources/public/js/components/news/form/main.js
+++ Resources/public/js/components/news/form/main.js
@@ -4,7 +4,8 @@ define(['text!./form.html'], function(form) {
 
         defaults: {
             templates: {
-                form: form
+                form: form,
+                url: '/admin/api/news<% if (!!id) { %>/<%= id %><% } %>'
             },
             translations: {
                 title: 'public.title',
@@ -35,6 +36,7 @@ define(['text!./form.html'], function(form) {
             this.render();
 
             this.bindDomEvents();
+            this.bindCustomEvents();
         },
 
         render: function() {
@@ -47,6 +49,34 @@ define(['text!./form.html'], function(form) {
             this.$el.find('input, textarea').on('keypress', function() {
                 this.sandbox.emit('sulu.header.toolbar.item.enable', 'save');
             }.bind(this));
+        },
+
+        bindCustomEvents: function() {
+            this.sandbox.on('sulu.toolbar.save', this.save.bind(this));
+        },
+
+        save: function(action) {
+            if (!this.sandbox.form.validate('#news-form')) {
+                return;
+            }
+
+            var data = this.sandbox.form.getData('#news-form');
+
+            this.sandbox.util.save(this.templates.url({id:null}), 'POST', data).then(function(response) {
+                this.afterSave(response, action);
+            }.bind(this));
+        },
+
+        afterSave: function(response, action) {
+            this.sandbox.emit('sulu.header.toolbar.item.disable', 'save');
+
+            if (action === 'back') {
+                this.sandbox.emit('sulu.router.navigate', 'example/news');
+            } else if (action === 'new') {
+                this.sandbox.emit('sulu.router.navigate', 'example/news/add');
+            } else if (!this.options.id) {
+                this.sandbox.emit('sulu.router.navigate', 'example/news/edit:' + response.id);
+            }
         }
     };
 });
