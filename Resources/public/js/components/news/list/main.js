b78373d5d466128cc09b9221721004abca7fd075
Johannes Wachter <johannes.wachter@massiveart.at>
2016-02-26 15:14:41 +0100

    added code to initialize the news list

--- Resources/public/js/components/news/list/main.js
+++ Resources/public/js/components/news/list/main.js
@@ -1,10 +1,86 @@
-define(function() {
+define(['text!./list.html'], function(list) {
 
-    'use strict';
+    var defaults = {
+        templates: {
+            list: list
+        }
+    };
 
     return {
+
+        defaults: defaults,
+
+        header: {
+            title: 'news.headline',
+            underline: false,
+
+            toolbar: {
+                buttons: {
+                    add: {},
+                    deleteSelected: {}
+                }
+            }
+        },
+
+        layout: {
+            content: {
+                width: 'max'
+            }
+        },
+
         initialize: function() {
-            this.$el.html('<h1>Hello awesome ' + this.options.name + ' world</h1>');
+            this.render();
+
+            this.bindDomEvents();
+            this.bindCustomEvents();
+        },
+
+        render: function() {
+            this.$el.html(this.templates.list());
+
+            this.sandbox.sulu.initListToolbarAndList.call(this,
+                'news',
+                '/admin/api/news/fields',
+                {
+                    el: this.$find('#list-toolbar-container'),
+                    instanceName: 'news',
+                    template: this.sandbox.sulu.buttons.get({
+                        settings: {
+                            options: {
+                                dropdownItems: [
+                                    {
+                                        type: 'columnOptions'
+                                    }
+                                ]
+                            }
+                        }
+                    })
+                },
+                {
+                    el: this.sandbox.dom.find('#news-list'),
+                    url: '/admin/api/news',
+                    searchInstanceName: 'news',
+                    searchFields: ['title', 'content'],
+                    resultKey: 'news-items',
+                    instanceName: 'news',
+                    actionCallback: this.toEdit.bind(this),
+                    viewOptions: {
+                        table: {
+                            actionIconColumn: 'title'
+                        }
+                    }
+                }
+            );
+        },
+
+        toEdit: function(id) {
+            this.sandbox.emit('sulu.router.navigate', 'example/news/edit:' + id);
+        },
+
+        bindDomEvents: function() {
+        },
+
+        bindCustomEvents: function() {
         }
     };
 });
